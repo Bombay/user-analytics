56b3f0ab80b78ceb6768cc0663853041fc5a5423
hyuntkim <hyuntkim@smilegate.com>
2023-03-17 16:05:46 +0900

    기본레이아웃 작업 중

---
+++ src/containers/navigation/Lnb.tsx
@@ -0,0 +1,11 @@
+import styled from '@emotion/styled'
+
+const LnbWrapper = styled.div`
+	background-color: gold;
+`
+
+function Lnb() {
+	return <LnbWrapper>test</LnbWrapper>
+}
+
+export default Lnb
--- src/layouts/DefaultLayout.tsx
+++ src/layouts/DefaultLayout.tsx
@@ -1,14 +1,20 @@
 import { ReactNode } from 'react'
+import { Grid } from '@mui/material'
+import Lnb from '@/containers/navigation/Lnb'
 
 function DefaultLayout({ children }: { children: ReactNode }) {
-	return <div>{children}</div>
+	return (
+		<Grid container>
+			<Grid item>
+				<Lnb />
+			</Grid>
+			<Grid item>{children}</Grid>
+		</Grid>
+	)
 }
 
 export default DefaultLayout
 
 DefaultLayout.getLayout = (page: ReactNode) => (
-	<DefaultLayout>
-		lnb...
-		{page}
-	</DefaultLayout>
+	<DefaultLayout>{page}</DefaultLayout>
 )
